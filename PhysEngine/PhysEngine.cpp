54e9f587dabc5f3361e841cf80f3cd9656070512
AvPa1212 <avpa1212@gmail.com>
2026-02-24 15:41:18 -0500

    Add project files.

---
+++ PhysEngine/PhysEngine.cpp
@@ -0,0 +1,12 @@
+﻿// PhysEngine.cpp : Defines the entry point for the application.
+//
+
+#include "PhysEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
